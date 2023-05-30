1c6e495f56465bdea12e71bdaf1ea13d373d9c39
soucs <soumya.sumswap@gmail.com>
2023-05-30 22:48:52 +0530

    New dataset histogram with scaling

--- glh_svm.py
+++ glh_svm.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -14,17 +15,22 @@ y = df['label']
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=None)
 
-# Principal Component Analysis
-pca = PCA().fit(X_train)
-Xpca = pca.transform(X_train)
-    
-# Explained variance ratio by each principal component
-v_ratio = pca.explained_variance_ratio_
-v_ratio = [v*100 for v in v_ratio]
-print('PCA:',v_ratio)
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-pca = PCA(n_components=3)
-X_train, X_test = pca.fit_transform(X_train), pca.transform(X_test)
+# # Principal Component Analysis
+# y = df['label']
+# pca = PCA().fit(X_train)
+# Xpca = pca.transform(X_train)
+    
+# # Explained variance ratio by each principal component
+# v_ratio = pca.explained_variance_ratio_
+# v_ratio = [v*100 for v in v_ratio]
+# print('PCA:',v_ratio)
+
+# pca = PCA(n_components=1)
+# X_train, X_test = pca.fit_transform(X_train), pca.transform(X_test)
 
 # SVM Classification
 svc = SVC(decision_function_shape='ovo', kernel='rbf',random_state=0)
